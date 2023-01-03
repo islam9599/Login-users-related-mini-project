1f6526f98a35be412f116a9ebad879a9abdf3f32
islam9599 <eislombek9599@gmail.com>
2023-01-03 13:50:47 +0900

    2nd update

--- src/components/Users/AddUsers.jsx
+++ src/components/Users/AddUsers.jsx
@@ -1,19 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../UI/Button.jsx";
 import Card from "../UI/Card.jsx";
 import classes from "./AddUsers.module.css";
 
-const AddUsers = () => {
+const AddUsers = (props) => {
+  const [userName, setUserName] = useState("");
+  const [userAge, setUserAge] = useState("");
   const addUser = (event) => {
     event.preventDefault();
+    if (userName.trim().length === 0 || userAge.trim().length === 0) {
+      return;
+    }
+    if (+userAge < 1) {
+      return;
+    }
+    props.onAddUser(userName, userAge);
+    setUserName("");
+    setUserAge("");
+  };
+  const userNameChange = (event) => {
+    setUserName(event.target.value);
+  };
+  const userAgeChange = (event) => {
+    setUserAge(event.target.value);
   };
   return (
     <Card className={classes.input}>
       <form action="" onSubmit={addUser}>
         <label htmlFor=" username">Username</label>
-        <input id="username" type="text" />
+        <input
+          id="username"
+          type="text"
+          value={userName}
+          onChange={userNameChange}
+        />
         <label htmlFor="age">Age</label>
-        <input id="age" type="number" />
+        <input
+          id="age"
+          type="number"
+          value={userAge}
+          onChange={userAgeChange}
+        />
         <Button type="submit">Add U ser</Button>
       </form>
     </Card>
